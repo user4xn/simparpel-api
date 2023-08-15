9d27ca6d6f1cc7e1f60b2ce4777f3c62447c48c9
Wildan Kurnia Candra <37010270+user4xn@users.noreply.github.com>
2023-08-16 00:59:22 +0700

    Leaflet Pelabuhan

---
+++ resources/js/Components/MapComponent.vue
@@ -0,0 +1,149 @@
+<template>
+    <div>
+      <select v-if="!drawingMode" @change="handleAreaChange" class="w-full border-0">
+        <option value="">Pilih Pelabuhan</option>
+        <option v-for="area in areas" :key="area.id" :value="area.id">{{ area.Name }}</option>
+      </select>
+      <div id="map" style="width: 100%; height: 400px;"></div>
+      <div class="grid grid-cols-3 gap-4 p-3">
+          <button v-if="!drawingMode" @click="startDrawingLayer" class="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Tambah Pelabuhan
+          </button>
+          <button v-if="drawingMode" @click="sendNewArea" class="mt-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+            Simpan
+          </button>
+          <button v-if="drawingMode" @click="cancelDrawing" class="mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            Batal
+          </button>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import L from 'leaflet';
+  import 'leaflet/dist/leaflet.css';
+  import axios from 'axios';
+  
+  export default {
+    mounted() {
+      this.initializeMap();
+      this.fetchAreas();
+    },
+    methods: {
+      initializeMap() {
+        this.map = L.map('map').setView([-2.4833826, 117.8902853], 5); // Default view at Indonesia
+  
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(this.map);
+      },
+      fetchAreas() {
+        axios.get('http://localhost:8000/api/location-area')
+          .then(response => {
+            const data = response.data;
+            if (data.status === 'success') {
+                this.areas = data.data;
+
+                this.areas.forEach(area => {
+                    const coordinates = area.coordinates.map(coord => [parseFloat(coord.lat), parseFloat(coord.long)]);
+                    const polygon = L.polygon(coordinates).addTo(this.map);
+                    const center = polygon.getBounds().getCenter();
+                    const marker = L.marker(center).addTo(this.map);
+                    marker.bindTooltip(area.Name, { permanent: true });
+                });
+            }
+          })
+          .catch(error => {
+            console.error('Error fetching areas:', error);
+          });
+      },
+      handleAreaChange(event) {
+        const selectedId = parseInt(event.target.value);
+        const selectedArea = this.areas.find(area => area.id === selectedId);
+        if (selectedArea) {
+          const coordinates = selectedArea.coordinates.map(coord => [parseFloat(coord.lat), parseFloat(coord.long)]);
+          const polygon = L.polygon(coordinates).addTo(this.map);
+          this.map.fitBounds(polygon.getBounds());
+
+          this.map.fitBounds(polygon.getBounds()).setZoom(16);
+        }
+      },
+      startDrawingLayer() {
+        this.drawingMode = true;
+
+        console.log('drawing mode', this.drawingMode);
+        this.polygonLayer = L.polygon([], { color: 'blue' }).addTo(this.map);
+
+        this.map.on('click', (e) => {
+            if (this.drawingMode) {
+            const lat = e.latlng.lat;
+            const lng = e.latlng.lng;
+            this.polygonLayer.addLatLng([lat, lng]);
+            }
+        });
+      },
+      cancelDrawing() {
+        this.drawingMode = false;
+
+        console.log('drawing mode', this.drawingMode);
+        this.map.off('click');
+        this.map.removeLayer(this.polygonLayer);
+      },
+      sendNewArea() {
+        this.drawingMode = false;
+        this.drawingMode = false;
+        const coordinates = this.polygonLayer.getLatLngs()[0];
+        if (coordinates.length < 3) {
+            alert('Polygon must have at least 3 vertices.');
+            return;
+        }
+
+        const areaName = prompt('Enter the name of the new area:');
+        if (!areaName) return;
+
+        axios.post('http://localhost:8000/api/location-area/upsert', {
+            area_id: null,
+            name: areaName,
+            coordinates: coordinates.map(coord => ({
+            lat: coord.lat.toFixed(6),
+            long: coord.lng.toFixed(6)
+            }))
+        }).then(response => {
+            if (response.status === 200) {
+            alert('New area added successfully');
+            }
+        }).catch(error => {
+            console.error('Error creating new area:', error);
+            alert('Error creating new area');
+        }).finally(() => {
+            this.map.off('click');
+            this.map.removeLayer(this.polygonLayer);
+        });
+      }
+    },
+    data() {
+      return {
+        map: null,
+        areas: [],
+        drawingMode: false,
+        polygonLayer: null,
+        areaName: ''
+      };
+    }
+  };
+  </script>
+  
+  <style>
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+  .leaflet-container {
+    height: 400px;
+    width: 100%;
+    max-width: 100%;
+    max-height: 100%;
+  }
+  </style>
+  
